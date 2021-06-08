43734a6db3b60f063fa0e941a351f59d0ea53c0d
Nicolas Rios <dnicolas.r98@gmail.com>
2021-06-08 11:40:24 +0200

    Do navbar component

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+const navbar = document.querySelector(".navbar");
+const navLinkWrapper = navbar.querySelector(".nav_links");
+const btnHamburguer = navbar.querySelector(".hamburguer");
+
+const displayNavbar = () => {
+    navbar.classList.toggle("active");
+    btnHamburguer.classList.toggle("active");
+    navLinkWrapper.classList.toggle("active");
+    console.log("click!");
+};
+
+btnHamburguer.addEventListener("click", displayNavbar)
