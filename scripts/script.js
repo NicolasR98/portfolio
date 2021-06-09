a52d35172dc7a5fc5cfba70e50fcd6dca61d4346
Nicolas Rios <dnicolas.r98@gmail.com>
2021-06-09 21:46:15 +0200

    Do 'myWork' section

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,31 @@
 const navbar = document.querySelector(".navbar");
 const navLinkWrapper = navbar.querySelector(".nav_links");
 const btnHamburguer = navbar.querySelector(".hamburguer");
+const projects = document.querySelectorAll(".project");
+
+projects.forEach((project) => {
+  const projectImg = project.querySelector(".project_img");
+  const projectLinksContainer = project.querySelector(
+    ".project_links_container"
+  );
+  const linksWrapper = project.querySelector(".links_wrapper");
+  projectImg.addEventListener("touchstart", (e) => {
+    projectImg.classList.add("active");
+    projectLinksContainer.classList.add("active");
+  });
+  linksWrapper.addEventListener("touchstart", (e) => {
+    projectImg.classList.remove("active");
+    projectLinksContainer.classList.remove("active");
+  });
+  projectImg.addEventListener("mouseenter", (e) => {
+    projectImg.classList.add("active");
+    projectLinksContainer.classList.add("active");
+  });
+  linksWrapper.addEventListener("mouseleave", (e) => {
+    projectImg.classList.remove("active");
+    projectLinksContainer.classList.remove("active");
+  });
+});
 
 const displayNavbar = () => {
   navbar.classList.toggle("active");
